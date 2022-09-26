59b72348ac6f4af8001bae9781e6608f73eb6e6f
LYYcreative <97815659+LYYcreative@users.noreply.github.com>
2022-09-26 21:29:02 +1000

    Update mpiadd.cpp

--- myself/mpiadd.cpp
+++ myself/mpiadd.cpp
@@ -10,22 +10,94 @@
 using namespace std::chrono;
 using namespace std;
 
-void randomVector(int vector[], int size)
+void vectorAdd(int vector[], int size)
 {
-    for (int i = 0; i < size; i++)
+	int num = 0;
+	for(int i = 0; i < size; i++)
+	{
+		num = i;
+        // to create a set of numbers that is neither increasing nor decrementing
+		if(num %2 == 0)
+		{
+			vector[i] = num -1;
+		}
+		else
+		{
+			vector[i] = num+1;
+		}
+	}
+}
+
+void qsort(int left, int right, int arr[])
+{
+    if(left>right){
+        return;
+    }
+    int i,j,base,temp;
+    i = left; j = right;
+    base = arr[left];
+
+    while(i<j)
     {
-        vector[i] = rand() % 100;
+        while(arr[j] >= base && i<j)
+            j--;
+        while(arr[j] <= base && i<j)
+            i++;
+
+        if(i<j)
+        {
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+
+        }
+    }
+    arr[left] = arr[i];
+    arr[i] = base;
+    qsort(left, i-1, arr);
+    qsort(i+1, right, arr);
+}
+
+void Merge(int left, int right, int arr[])
+{
+    int mid = (left+right)/2;
+    
+    //compare values in each sub-vector and replace them to right place
+    int i=left, j=mid+1, k=0; 
+    int *temp = (int *) malloc((right-left+1) * sizeof(int *));
+    
+    while(i<=mid&&j<=right)
+    {
+        if(arr[i]<=arr[j]) 
+            temp[k++]=arr[i++];
+        else
+            temp[k++]=arr[j++];
+    }
+    while(i<=mid)
+        temp[k++]=arr[i++];
+    while(j<=right)
+        temp[k++]=arr[j++];
+    for(i=left,k=0;i<=right;i++,k++)
+		arr[i]=temp[k];
+    delete []temp;
+
+    return;
+
+}
+
+void Print(int arr[], int size)
+{
+    for(int i = 0; i<size; i++)
+    {
+        cout << arr[i] << " ";
     }
 }
 
+int main(int argc, char** argv)
+{
+    int size = 16;
+    int *v1, *sub_v1, *result;
 
-int main(int argc, char** argv){
-
-    unsigned long size = 100000000;
-
-    srand(time(0));
-
-    int *v1, *v2, *v3, *v1_sub, *v2_sub, *v3_sub, totalSum;
     int numtasks, rank, name_len, tag = 1;
     int res;
 
@@ -40,49 +112,47 @@ int main(int argc, char** argv){
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
     int length = size/numtasks;
-    int localSum = 0;
 
-    v1_sub = (int *) malloc(length * sizeof(int *));
-    v2_sub = (int *) malloc(length * sizeof(int *));
-    v3_sub = (int *) malloc(length * sizeof(int *));
-
+    sub_v1 = (int *) malloc(length * sizeof(int *));
 
     if(rank == 0)
     {
         v1 = (int *) malloc(size * sizeof(int *));
-        v2 = (int *) malloc(size * sizeof(int *));
-        v3 = (int *) malloc(size * sizeof(int *));
+        result = (int *) malloc(size * sizeof(int *));
 
-        randomVector(v1, size);
-        randomVector(v2, size);
+        vectorAdd(v1,size);
+        cout << "Origenal Vector: ";
+        Print(v1,size);
+
     }
     auto start = high_resolution_clock::now();
 
-    MPI_Scatter(v1, length, MPI_INT, v1_sub, length, MPI_INT, 0,MPI_COMM_WORLD);
+    MPI_Scatter(v1, length, MPI_INT, sub_v1, length, MPI_INT, 0,MPI_COMM_WORLD);
 
-    MPI_Scatter(v2, length, MPI_INT, v2_sub, length, MPI_INT, 0,MPI_COMM_WORLD);
+    qsort(0,length-1,sub_v1);
 
-
-    for (size_t i = 0; i < length; i++)
-    {
-        v3_sub[i] += v1_sub[i] + v2_sub[i];
-        localSum += v3_sub[i];
-    }
-
-    MPI_Gather(v3_sub, length, MPI_INT, v3, length, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Reduce(&localSum, &totalSum, 1, MPI_INT, MPI_SUM, 0,MPI_COMM_WORLD);
+    MPI_Gather(sub_v1, length, MPI_INT, result, length, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Barrier(MPI_COMM_WORLD);
 
     if(rank == 0)
     {
+        Merge(0,(length*2)-1,result);
+        Merge(length*2,size-1,result);
+        Merge(0,size-1,result);
+
         auto stop = high_resolution_clock::now();
         auto duration = duration_cast<microseconds>(stop - start);
-        cout << "Time taken by function: "
-         << duration.count() << " microseconds" 
-         <<" and the total sum is " << totalSum
-         << endl;
+        
+        cout << "\n" << "final Vector: ";
+        Print(result,size);
+        cout << "\n";
 
+        cout << "Time taken by function: "
+         << duration.count() << " microseconds"
+         << endl;
     }
     
 
     MPI_Finalize();
+
 }

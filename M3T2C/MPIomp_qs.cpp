16397cb4d0172b6cc5bbb82d44a146c25988bcd6
LYYcreative <97815659+LYYcreative@users.noreply.github.com>
2022-10-11 13:31:03 +1100

    Create MPIomp_qs.cpp

---
+++ M3T2C/MPIomp_qs.cpp
@@ -0,0 +1,172 @@
+#include <iostream>
+#include <mpi.h>
+#include <stdio.h>
+#include <cstring>
+#include <cstdlib>
+#include <time.h>
+#include <chrono>
+#include <omp.h>
+
+
+using namespace std::chrono;
+using namespace std;
+
+//function to initial the origenal vector
+void vectorAdd(int vector[], int size)
+{
+	int num = 0;
+	for(int i = 0; i < size; i++)
+	{
+		num = i;
+        // to create a set of numbers that is neither increasing nor decrementing
+		if(num %2 == 0)
+		{
+			vector[i] = num -1;
+		}
+		else
+		{
+			vector[i] = num+1;
+		}
+	}
+}
+//quick sort
+void qsort(int left, int right, int arr[],int numtasks)
+{
+    if(left>right){
+        return;
+    }
+    int i,j,base,temp;
+    i = left; j = right;
+    base = arr[left];//let the left most element as basis
+
+    //j moving from right to left, i moving from left to right
+    //if find an array[j] less than base-value and an array[i] greater than base-value, swap array[i] and array[j]
+    int threads = numtasks;
+    #pragma omp paralell num_threads(threads)
+    {
+        while(i<j)
+        {
+            while(arr[j] >= base && i<j)
+                j--;
+            while(arr[j] <= base && i<j)
+                i++;
+
+            if(i<j)
+            {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+
+            }
+        }
+    }
+    
+	
+    //puts the base value to the right place
+    arr[left] = arr[i];
+    arr[i] = base;
+    qsort(left, i-1, arr,threads);
+    qsort(i+1, right, arr,threads);
+}
+//merge any two sequential parts
+void Merge(int left, int right, int arr[])
+{
+    int mid = (left+right)/2;
+    
+    //compare values in each sub-vector and replace them to right place
+    int i=left, j=mid+1, k=0; 
+    int *temp = (int *) malloc((right-left+1) * sizeof(int *));
+
+    
+    while(i<=mid&&j<=right)
+    {
+        if(arr[i]<=arr[j]) 
+            temp[k++]=arr[i++];
+        else
+            temp[k++]=arr[j++];
+    }
+    
+    while(i<=mid)
+        temp[k++]=arr[i++];
+    while(j<=right)
+        temp[k++]=arr[j++];
+    for(i=left,k=0;i<=right;i++,k++)
+		arr[i]=temp[k];
+    delete []temp;
+
+    return;
+
+}
+//print the vector
+void Print(int arr[], int size)
+{
+    for(int i = 0; i<size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
+int main(int argc, char** argv)
+{
+    int size = 16;
+    int *v1, *sub_v1, *result;
+
+    int numtasks, rank, name_len, tag = 1;
+    int res;
+
+    MPI_Status status;
+    // Initialize the MPI environment
+    MPI_Init(&argc,&argv);
+
+    // Get the number of tasks/process
+    MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+
+    // Get the rank
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    int length = size/numtasks;
+
+    sub_v1 = (int *) malloc(length * sizeof(int *));
+
+    if(rank == 0)
+    {
+        v1 = (int *) malloc(size * sizeof(int *));
+        result = (int *) malloc(size * sizeof(int *));
+
+        vectorAdd(v1,size);
+        cout << "Origenal Vector: ";
+        Print(v1,size);
+
+    }
+    auto start = high_resolution_clock::now();
+    //scatter the origenal vector v1
+    MPI_Scatter(v1, length, MPI_INT, sub_v1, length, MPI_INT, 0,MPI_COMM_WORLD);
+
+    qsort(0,length-1,sub_v1,numtasks);//sort each sub_v1
+
+    MPI_Gather(sub_v1, length, MPI_INT, result, length, MPI_INT, 0, MPI_COMM_WORLD);//gather each sub_v1 and put them to a result vector
+    MPI_Barrier(MPI_COMM_WORLD);//wait for all tasks end
+
+    if(rank == 0)
+    {
+        Merge(0,(length*2)-1,result);
+        Merge(length*2,size-1,result);
+        Merge(0,size-1,result);
+	//merge every two sequential sets
+
+        auto stop = high_resolution_clock::now();
+        auto duration = duration_cast<microseconds>(stop - start);
+        
+        cout << "\n" << "Sorted Vector: ";
+        Print(result,size);
+        cout << "\n";
+
+        cout << "Time taken by function: "
+         << duration.count() << " microseconds"
+         << endl;
+    }
+    
+
+    MPI_Finalize();
+
+}
